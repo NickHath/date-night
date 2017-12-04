d68189a8504c3b1433477322be267da4840c23e2
alex tingey <alextingey1@gmail.com>
2017-12-04 15:20:12 -0700

    added schema and sql queries

---
+++ db/add_date.sql
@@ -0,0 +1,2 @@
+insert into dates (date_id, title, first_buisness, second_buisness, third_buisness)
+values ($1, $2, $3, $4, $5);
--- schema.sql
+++ schema.sql
@@ -1,13 +1,10 @@
-create businesses(
-    
-)
 
 create table dates(
     id serial primary key not null,
+    date_id bigint not null,
     title varchar(25) not null,
-    first_buisness varchar(255),
+    first_buisness varchar(255) not null,
     second_buisness varchar(255),
-    third_buisness varchar(255),
-    geographical_location varchar(255),
-    foreign key(first_buisness) references ()
+    third_buisness varchar(255)
 )
+
