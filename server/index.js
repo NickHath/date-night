d3f6d50208e5a9bd602e8bcddbae3c7502f59661
Nick <Nicholashathaway@gmail.com>
2017-12-04 11:58:35 -0700

    remove session initialization

--- server/index.js
+++ server/index.js
@@ -8,17 +8,11 @@ const bodyParser = require("body-parser")
 const app = express();
 
 app.use(bodyParser.json());
-app.use(session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-}))
 
 massive(process.env.CONNECTION_STRING).then((db) => {
     console.log("Database connected")
     app.set('db', db)
 })
-
 
 const PORT = 4200;
 app.listen(PORT, console.log(`I'm listening.. port: ${PORT}`));
