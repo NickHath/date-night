bfa31728fc27ea82d6421fd90f7be1f70e592570
Nick <Nicholashathaway@gmail.com>
2017-12-07 16:15:39 -0700

    add comments to DateResults

--- src/components/DateResults/DateResults.js
+++ src/components/DateResults/DateResults.js
@@ -19,10 +19,12 @@ class DateResults extends Component {
     }
   }
 
+  // initializes state (which sets off the first randomizer method)
   componentDidMount() {
     this.initState();
   }
 
+  // sets state to appropriate length for all 4 arrays
   initState() {
     let durations = { 'short': 1, 'medium': 2, 'long': 3 };
     let locked = [], businesses = [], categories = [];
@@ -36,37 +38,19 @@ class DateResults extends Component {
     });
   }
 
+  // this function sets off all the callback methods to update state
+  // called in initState and whenever they press the refresh button
   refreshDate() {
-    let { location, radius, duration, startTime } = this.props.preferences;
     this.getRandomCategories(this.props.preferences.startTime);    
   }
 
-  findBusinesses() {
-    let newBusinesses = [];
-    this.state.lockedBusinesses.forEach((locked, index) => {
-      let location = this.props.preferences.location;
-      let currCategory = this.state.categories[index];
-      if (!locked) {
-        if (Object.keys(this.props.results).includes(currCategory)) {
+  updateBusinesses() {
 
-        } else {
-          this.props.getResults(location, currCategory);
-        }
-        console.log('find new business with' + this.state.categories[index]);
-
-      }
-    })
-  }
-
-  randomBusiness(businesses) {
-    let randIndex = Math.floor(Math.random() * businesses.length)
-    return businesses[randIndex];
   }
 
   // selects appropriate number of random categories for startime
   getRandomCategories(startTime) {
     let newCategories = [...this.state.categories];
-
     // only get a random category when category at that index is not locked
     this.state.lockedCategories.forEach((locked, index) => {
       if (!locked) {
@@ -74,10 +58,9 @@ class DateResults extends Component {
       }
       startTime += 200;         
     });
-
     // after updateing categories, get new business with those keywords
     this.setState({ categories: newCategories }, () => {
-      this.findBusinesses();
+      this.updateBusinesses();
     });
   }
 
@@ -96,40 +79,25 @@ class DateResults extends Component {
     return mainCategories[randIndex];
   }
 
+  // given an index, updates lockedBusinesses at that index
   lockBusiness(index) {
     let lockedBusinesses = [...this.state.businesses];
     lockedBusinesses[index] = true;
     this.setState({ lockedBusinesses });
   }
 
-  // given an index and a subcategory, update state
+  // given an index and a subcategory, update categories AND lockedCategories
+  // at that index
   lockCategory(index, newCategory) {
     let lockedCategories = [...this.state.lockedCategories];
     let categories = [...this.state.categories];
-
     lockedCategories[index] = true;
     categories[index] = newCategory;
-
     this.setState({ lockedCategories, categories });
   }
 
   render() {
-    // const displayResults = this.props.results.map(results => {
-    //   if (results.length === 0) {
-    //     return <div><h2>No results found for this category</h2></div>;
-    //   } else {
-    //     let randIndex = Math.floor(Math.random() * results.length);
-    //   return (
-    //     <div>
-          
-    //       <a target="_blank" href={ results[randIndex].url }>{results[randIndex].name}</a>
-    //       {results[randIndex].categories.map(category => <h2>{category.alias}
-    //       </h2>)}
-    //     </div>
-    // );
-    //   }
-    // })
-    console.log('STATE:', this.state);
+    console.log('CATEGORIES:', this.state.categories);
     return (
       <div className='date-results'>
         <h1>All results from our date search</h1>
