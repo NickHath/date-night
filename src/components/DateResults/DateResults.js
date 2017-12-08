e748ec44e8c620a73d6226551195d618f172e156
Nick <Nicholashathaway@gmail.com>
2017-12-07 19:17:09 -0700

    add debugging code for redux-promise-middleware issue

--- src/components/DateResults/DateResults.js
+++ src/components/DateResults/DateResults.js
@@ -6,7 +6,7 @@ import Date from './Date';
 
 // redux
 import { connect } from 'react-redux';
-import { getResults, clearResults } from '../../ducks/reducer';
+import { getResults } from '../../ducks/reducer';
 
 class DateResults extends Component {
   constructor() {
@@ -44,8 +44,16 @@ class DateResults extends Component {
     this.getRandomCategories(this.props.preferences.startTime);    
   }
 
+  // using categories on state, retrieve a random business for each non-locked business on state
   updateBusinesses() {
     console.log('update our businesses with these categories:\n', this.state.categories);
+    let { location, radius } = this.props.preferences;
+    
+    this.state.lockedBusinesses.forEach((locked, index) => {
+      if (!locked) {
+        this.props.getResults(location, this.state.categories[index], radius);
+      }
+    })
   }
 
   // selects appropriate number of random categories for startime
@@ -97,13 +105,15 @@ class DateResults extends Component {
   }
 
   render() {
-    // console.log('CATEGORIES:', this.state.categories);
-    console.log(this.state);
+    console.log('CATEGORIES:', this.state.categories);
+    console.log('BUSINESSES:', this.state.businesses);
+    console.log('RESULTS:', this.props.results);
     return (
       <div className='date-results'>
         <h1>All results from our date search</h1>
         {/* render several date components here */}
         <button onClick={ () => this.refreshDate() }>Give me some dates!!!</button><br/><br/>
+
         <button onClick={ () => this.lockCategory(0, '1 locked!!!') }>Lock Category #1</button>
         <button onClick={ () => this.lockCategory(1, '2 locked!!!') }>Lock Category #2</button>
         <button onClick={ () => this.lockCategory(2, '3 locked!!!') }>Lock Category #3</button><br/>
@@ -124,4 +134,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, { getResults, clearResults })(DateResults);
+export default connect(mapStateToProps, { getResults })(DateResults);
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -2,21 +2,25 @@ import axios from 'axios';
 import categories from './categories';
 
 const initialState = {
-  results: [],
+  results: {},
   preferences: {},
-  categories: categories
+  categories: categories,
 }
 
 const GET_RESULTS = 'GET_RESULTS'
-    , CLEAR_RESULTS = 'CLEAR_RESULTS'
     , ADD_PREFERENCES = 'ADD_PREFERENCES';
 
 export default function reducer(state = initialState, action) {
+  console.log(state);
   switch(action.type) {
-    case GET_RESULTS + '_FULFILLED': 
-      return Object.assign({}, state, { results: [...state.results, action.payload] });
-    case CLEAR_RESULTS:
-      return Object.assign({}, state, { results: [] })
+    case GET_RESULTS + '_PENDING':
+      console.log('GET_RESULTS_PENDING:', action);
+      return state;
+    case GET_RESULTS + '_FULFILLED':
+      // let newResults = {};
+      // newResults[action.category] = action.payload;
+      console.log('GET_RESULTS_FULFILLED:', action);
+      return state;
     case ADD_PREFERENCES:
       return Object.assign({}, state, { preferences: action.payload })
     default:
@@ -25,25 +29,20 @@ export default function reducer(state = initialState, action) {
 }
 
 export function getResults(location, category, radius) {
+  console.log('GET RESULTS', location, category, radius);
   const results = axios.post('http://localhost:4200/api/yelp', { location, category, radius })
-                          .then(res => res.data);
+                       .then(res => res.data);
+  
   return {
     type: GET_RESULTS,
-    payload: results
+    payload: results,
+    category: category,
   }
 }
 
-export function clearResults() {
-  return {
-    type: CLEAR_RESULTS
-  }
-}
-
-export function addPreferences(preferences){
+export function addPreferences(preferences) {
   return {
     type: ADD_PREFERENCES,
-    payload: preferences
+    payload: preferences,
   }
 }
-
-
