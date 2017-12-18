6b6f0d33d65f5ffd481dcb636efc1b7194338471
Nick <Nicholashathaway@gmail.com>
2017-12-18 14:35:57 -0700

    add popular view where you can see dates for your area

---
+++ src/components/PopularDates/PopularDates.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getPopularDates } from '../../ducks/reducer';
+
+class PopularDates extends Component {
+  handleClick() {
+    this.props.getPopularDates(this.refs['location'].value);
+  }
+
+  render() {
+    console.log(this.props);
+    const allDates = this.props.popularDates.map(date => {
+      return (
+        <Link to={ `results/${date.date_id}` }>
+          <div>
+            { date.first_business } { date.second_business } { date.third_business }
+          </div>
+        </Link>
+      );
+    });
+    console.log(allDates);
+    return (
+      <div className='popular-dates-wrapper'>
+        <input ref='location'/>
+        <button onClick={ () => this.handleClick() }>Get Dates</button>
+        { allDates }
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { popularDates: state.popularDates };
+}
+
+export default connect(mapStateToProps, { getPopularDates })(PopularDates);
