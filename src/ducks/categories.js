10eec9d8994c74efa9d9b58d0a77ded90e165886
Alex Tingey <alextingey1@gmail.com>
2017-12-28 19:30:49 -0700

    minor tweak to categories

--- src/ducks/categories.js
+++ src/ducks/categories.js
@@ -1,5 +1,5 @@
 // main categories and select subcategories
 module.exports = {
-  'day': ['active', 'arts', 'coffee', 'media', 'galleries', 'antiques', 'thrift_stores', 'parks', 'restaurant'],
+  'day': ['active', 'arts', 'coffee', 'media', 'galleries', 'antiques', 'parks', 'restaurant'],
   'night': ['nightlife', 'coffee', 'icecream', 'parks', 'restaurant']
 }
